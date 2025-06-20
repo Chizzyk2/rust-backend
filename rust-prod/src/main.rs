e1dfc919a44897971e205aa3975b2e39a93606c2
Chizzyk2 <ziterenna@gmail.com>
2025-06-20 22:00:19 +0100

    Rust-backend

--- rust-prod/src/main.rs
+++ rust-prod/src/main.rs
@@ -1,3 +1,26 @@
-fn main(){
-    println!("Hello world!")
+#[allow(unused_imports)]
+use axum::{routing::get, serve::Listener, Router};
+
+#[tokio::main]
+
+async fn main() {
+    let addr = "127.0.0.1:3000";
+    let listener = tokio::net::TcpListener::bind(addr)
+    .await
+    .unwrap();
+
+    println!("Server running at {addr:?}");
+
+    axum::serve(listener, router())
+        .await
+        .unwrap();
+}
+
+fn router() -> Router {
+    Router::new().route("/", get(hello_world))
+
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, world! from axum"
 }
