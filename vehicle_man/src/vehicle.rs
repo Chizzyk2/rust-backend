df77cf9faefe402c198f40a9d184225d61872da0
Chizzyk2 <ziterenna@gmail.com>
2025-06-24 07:21:04 +0100

    rust-backend

--- vehicle_man/src/vehicle.rs
+++ vehicle_man/src/vehicle.rs
@@ -1,12 +1,13 @@
-use axum::Json;
+use axum::{extract::Query, Json};
+use uuid::uuid;
 
 
-#[derive(Debug, serde::Serialize)]
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Vehicle{
     manufacturer: String,
     model: String,
     year: u32,
-    id: String
+    id: Option<String>
 
 }
 
@@ -17,11 +18,30 @@ pub async fn vehicle_get() -> Json<Vehicle>{
             manufacturer : "Doge".to_string(),
             model: "RAM 1500".to_string(),
             year: 2021,
-            id: uuid::Uuid::new_v4().to_string()
+            id: Some(uuid::Uuid::new_v4().to_string())
         }
     )
 }
 
-pub async fn vehicle_post(){
+// pub async fn vehicle_post(Json(mut v): Json<Vehicle>) -> Json<Vehicle>{
+//     println!("manufacturer: {0}, model:{1}, year{2}", v.manufacturer, v.model, v.year);
+//     v.id = Some(uuid::Uuid::new_v4().to_string());
+//     Json::from(v)
+// }
 
+#[derive(Debug, serde::Deserialize)]
+
+pub struct Customer{
+    first_name: String,
+    last_name: String
+}
+
+pub async fn vehicle_post(
+    Query(mut v): Query<Vehicle>,
+    Query(c): Query<Customer>
+) -> Json<Vehicle>{
+    println!("manufacturer: {0}, model: {1}, year: {2}", v.manufacturer, v.model, v.year);
+    v.id = Some(uuid::Uuid::new_v4().to_string());
+    println!("First name is {0}\nLast name is {1}", c.first_name, c.last_name);
+    Json(v)
 }
--- vehicle_man/target/api-tests.ps1
+++ vehicle_man/target/api-tests.ps1
@@ -1,2 +1,55 @@
+# Get/read vehicle info
+
 Invoke-RestMethod -Uri http://localhost:4000/vehicle -Method Get
+
+# Create/ post vehicle info 
+
 Invoke-RestMethod -Uri http://localhost:4000/vehicle -Method Post
+
+$Params = @{
+    Uri = 'http://localhost:4000/vehicle'
+    Method = 'Post'
+    Body = @{
+        manufacturer = 'Tesla'
+        model = 'Model Y'
+        year = 2024
+    } | ConvertTo-Json
+    ContentType = 'application/json'
+}
+
+Invoke-RestMethod @Params
+
+
+# using query....
+
+$Params = @{
+    Uri = 'http://localhost:4000/vehicle?manufacturer=Tesla&model=Model Y&year=2024'
+    Method = 'Post'
+}
+
+Invoke-RestMethod @Params
+
+
+# Customer query
+
+$Params = @{
+    Uri = 'http://localhost:4000/vehicle'
+    Method = 'Post'
+    Body = @{
+        manufacturer = 'Tesla'
+        model = 'Model Y'
+        year = 2024
+        first_name = 'Mike'
+        last_name = 'Uche'
+    } | ConvertTo-Json
+    ContentType = 'application/json'
+}
+
+Invoke-RestMethod @Params
+
+$Params = @{
+    Uri = 'http://localhost:4000/vehicle?manufacturer=Tesla&model=Model Y&year=2024&first_name=Mike&last_name=Uche'
+    Method = 'Post'
+}
+
+Invoke-RestMethod @Params
